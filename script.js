0d61279f819c55e1bf9cca21fb9cbe89b667f615
pank <lucasfelicio4ever@email.com>
2021-02-03 21:13:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const message = ["We do not check the refrigerator multiple times to find new food, we check to see if our standards have dropped enough to eat what was available.",
+"Gummy worms have more bones in them than actual worms.",
+"Most of the trash you see on the ground is unhealthy food packaging because people who don’t care about their own health don’t care about the Earth’s either.",
+"In a span of a few years our parents go from telling us 'sex is bad'to 'I want grandkids'.",
+"Being with people who don't listen is lonelier than being alone.",
+"Mosquitoes sure are brave for creatures with only 1 hp.",
+"Light bulbs were such a good idea that they became a symbol for a good idea.",
+"As a child, it seemed like parents had eyes on the back of their heads. As adults, it’s clear that little kids are extremely predictable.",
+"Maybe superheroes wear capes to hide the zipper on the back of their onesie.",
+"Elsa is pretty athletic for a person that was locked up in her room since she was young until she turned 21."];
+
+console.log(message[Math.floor(Math.random() * 10)]);
